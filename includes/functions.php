f977603b853bf527660493fe1fdb90e089cf5b41
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2022-01-29 12:27:38 -0500

    Added functions, validations and submit handling.

--- includes/functions.php
+++ includes/functions.php
@@ -1,2 +1,43 @@
 <?php
+// Count entries in post array, if more than 0 - show submission data. If not return false because it's empty.
+function showSubmission(){
+    return count($_POST) > 0;
+}
 
+//Get the data that the user submitted into the _POST array
+function getPostData($key){
+    if (array_key_exists($key, $_POST)) {
+        return $_POST[$key];
+    }
+    return "";
+}
+
+//Get error messages from session
+function getErrors($key){
+
+    if (! isset($_SESSION)){
+        return "";
+    }
+    if (! array_key_exists('errors', $_SESSION)){
+        return "";
+    }
+    if (! array_key_exists($key, $_SESSION['errors'])) {
+        return "";
+    }
+    return $_SESSION['errors'][$key];
+}
+
+//Get the user submission from session
+function getSubmission($key){
+
+    if (! isset($_SESSION)){
+        return "";
+    }
+    if (! array_key_exists('submission', $_SESSION)){
+        return "";
+    }
+    if (! array_key_exists($key, $_SESSION['submission'])) {
+        return "";
+    }
+    return $_SESSION['submission'][$key];
+}
--- public/contact.php
+++ public/contact.php
@@ -1,12 +1,23 @@
 <?php
 session_start();
-
-//$firstName
-//$lastName
-//$email
-//$message
-
 include '../includes/functions.php';
+
+$firstName = getSubmission('first_name');
+$lastName = getSubmission('last_name');
+$email = getSubmission('email');
+$comment = getSubmission('comment');
+
+$showSubmissionData = showSubmission();
+
+//Get errors if they exist and store them to display
+$firstNameError = getErrors('first_name');
+$lastNameError = getErrors('last_name');
+$emailError = getErrors('email');
+$commentError = getErrors('comment');
+
+//When user refreshes the page the error messages clear
+$_SESSION['errors'] = [];
+$_SESSION['submission'] =[];
 
 ?>
 
@@ -17,14 +28,32 @@ include '../includes/functions.php';
     </head>
     <body>
         <?php include '../includes/partials/navigation.php';?>
+        
+
         <form method="POST" action="../includes/submit.php">
             <input type="text" placeholder="First Name" name="first_name" value="<?php echo $firstName; ?>">
+            <!-- Small bit of css to style errors -->
+            <span style="color: red;">
+                    <?php echo $firstNameError; ?>
+                </span>
             <br>
             <input type="text" placeholder="Last Name" name="last_name" value="<?php echo $lastName; ?>">
+            <span style="color: red;">
+                    <?php echo $lastNameError; ?>
+                </span>
             <br>
             <input type="text" placeholder="Email" name="email" value="<?php echo $email; ?>">
+            <span style="color: red;">
+                    <?php echo $emailError; ?>
+                </span>
+            <br>
+            <input name="comment" placeholder="Enter Message Here!" value="<?php echo $comment; ?>"></input>
+            <span style="color: red;">
+                    <?php echo $commentError; ?>
+                </span>
             <br>
             <button type="submit">Submit</button>
         </form>
+      
     </body>
 </html>
