e36a39dd7e211cbeb27c1328ab9a046b74c21d9e
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2022-01-29 14:05:32 -0500

    Added Array List and configured to display

--- includes/partials/navigation.php
+++ includes/partials/navigation.php
@@ -1,9 +1,9 @@
 <header>
     <nav>
         <ul id="navigation">
-            <li><a href="index.php">HOME</a></li>
-            <li><a href="listings.php">SECOND PAGE</a></li>
-            <li><a href="contact.php">CONTACT</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="listings.php">Portfolio</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
 </header>
--- public/listings.php
+++ public/listings.php
@@ -1,6 +1,32 @@
 <?php
-
 include '../includes/functions.php';
+
+$projects = [
+    [
+        'project' => 'Item 1',
+        'preview' => 'images/project1.png'
+    ],
+    [
+        'project' => 'Item 2',
+        'preview' => 'images/project2.png'
+    ],
+    [
+        'project' => 'Item 3',
+        'preview' => 'images/project3.png'
+    ],
+    [
+        'project' => 'Item 4',
+        'preview' => 'images/project4.png'
+    ],
+    [
+        'project' => 'Item 5',
+        'preview' => 'images/project5.png'
+    ],
+    [
+        'project' => 'Item 6',
+        'preview' => 'images/project6.png'
+    ]
+];
 
 ?>
 <html>
@@ -10,5 +36,14 @@ include '../includes/functions.php';
     </head>
     <body>
         <?php include '../includes/partials/navigation.php';?>
+        <h1>Listing</h1>
+        <?php
+            //for each item listed in the array, can call individual keys as $items
+            foreach($projects as $item){
+                //Echo all images and titles onto the page
+                echo $item['project'];
+                echo '<img src="' . $item['preview'] . '"/>';
+            }
+        ?>
     </body>
 </html>
